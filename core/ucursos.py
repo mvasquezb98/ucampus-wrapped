c9cff58b05e66008fd73ef48f7df3ce075a53eef
Mariana Vásquez <marianavasquez@ug.uchile.cl>
2025-08-13 19:00:27 -0400

    Actualización de la ruta de salida del scrapping de u-cursos

--- core/ucursos.py
+++ core/ucursos.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from selenium.webdriver.common.by import By
 import time
-import openpyxl
+import os
 
 def urls_cursos(driver):
     urls_cursos_alumno = []
@@ -98,7 +98,10 @@ def extraer_datos_ucursos(driver,urls_cursos_alumno):
   df_notas = pd.DataFrame(notas_data)
   df_actas = pd.DataFrame(acta_data)
   # Guardar en Excel con varias hojas
-  with pd.ExcelWriter("datos_ucursos.xlsx", engine="openpyxl") as writer:
+  base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+  data_path = os.path.join(base_path,'datos', 'output')
+  salida = os.path.join(data_path,"datos_ucursos.xlsx")
+  with pd.ExcelWriter(salida, engine="openpyxl") as writer:
       df_notas.to_excel(writer, sheet_name="Notas_ucursos", index=False)
       df_actas.to_excel(writer, sheet_name="Actas_ucursos", index=False)
   print("✅ Datos exportados correctamente a 'datos_ucursos.xlsx'")
